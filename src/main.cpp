f8849b3dbd4814b10057a0cbffab1aed0f4edaa8
Alexey Nikolaichik <rogroty@gmail.com>
2025-08-23 13:22:07 +0300

    Готовый начальный проект. Треугольник

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,142 @@
 
 #include <iostream>
 
+int windowWidth = 800;
+int windowHeight = 600;
+
+GLfloat points[]
+{
+	-0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,
+	0.0f, 0.5f, 0.0f,		0.0f, 1.0f, 0.0f,
+	0.5f, -0.5f, 0.0f,		0.0f, 0.0f, 1.0f,
+};
+
+const char* vertShader = 
+"#version 460 core\n"
+"layout(location = 0) in vec3 vertexPos;\n"
+"layout(location = 1) in vec3 vertexCol;\n"
+"out vec3 color;\n"
+"void main() {\n"
+"    color = vertexCol;\n"
+"    gl_Position = vec4(vertexPos, 1.0f);\n"
+"}\n";
+
+const char* fragShader = 
+"#version 460 core\n"
+"in vec3 color;\n"
+"out vec4 fragColor;\n"
+"void main() {\n"
+"    fragColor = vec4(color, 1.0f);\n"
+"}\n";
+
+void changeWindowSizeCallback(GLFWwindow *window, int width, int height)
+{
+	windowWidth = width;
+	windowHeight = height;
+	glViewport(0, 0, windowWidth, windowHeight);
+}
+
+void keyHandlerCallback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		std::cout << "Window has closed!\n";
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
+
 int main(void)
 {
-	GLFWwindow *window;
-
 	if (!glfwInit())
+	{
+		std::cout << "Failed to initialized GLFW\n";
 		return -1;
+	}
 
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL Tutorial", nullptr, nullptr);
 	if (!window)
 	{
+		std::cout << "Failed to create window\n";
 		glfwTerminate();
 		return -1;
 	}
+
+	glfwSetWindowSizeCallback(window, changeWindowSizeCallback);
+	glfwSetKeyCallback(window, keyHandlerCallback);
 
 	glfwMakeContextCurrent(window);
 
 	if (!gladLoadGL(glfwGetProcAddress))
 	{
 		std::cout << "Failed to inisialized GLAD\n";
+		glfwTerminate();
 		return -1;
 	}
 
+	std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
 	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
-	glClearColor(0.0f, 0.2f, 0.5f, 1.0f);
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertShader, nullptr);
+	glCompileShader(vs);
+
+	GLint success;
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		char log[512];
+		glGetShaderInfoLog(vs, 512, nullptr, log);
+		std::cerr << "Vertex shader error:\n" << log << std::endl;
+	}
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragShader, nullptr);
+	glCompileShader(fs);
+
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		char log[512];
+		glGetShaderInfoLog(fs, 512, nullptr, log);
+		std::cerr << "Vertex shader error:\n" << log << std::endl;
+	}
+
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (void*)0);
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (void*)(3 * sizeof(GL_FLOAT)));
+	
+	glClearColor(0.0f, 0.2f, 0.2f, 1.0f);
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(program);
+		glBindVertexArray(vao);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 
